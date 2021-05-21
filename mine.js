9088907463ce7f714ff4a3cb310dff86c1f4828e
adelahmed <aadil0384@gmail.com>
2021-05-21 09:51:08 +0200

    commit

---
+++ mine.js
@@ -0,0 +1,8 @@
+function changeNavColor(){
+    if(window.scrollY>1000){
+        document.getElementById("nav").style.backgroundColor="black";
+    }
+    else{
+        document.getElementById("nav").style.backgroundColor="transparent";
+    }
+}
